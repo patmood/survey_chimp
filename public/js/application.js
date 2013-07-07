6d2b687398cf25f36467fc97d662ee0a4aa7dd1c
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-07 16:29:26 -0500

    placeholder replace, clean up css

--- public/js/application.js
+++ public/js/application.js
@@ -5,5 +5,15 @@ $(document).ready(function() {
   //   var request = $.ajax({ url: $(this).attr('href'), type: 'delete' });
   //   request.done(function () { window.location = "/"; });
   // });
-    
+
+
+  $(document).on('focusin', 'input', function(){
+    $(this).attr('data-placeholder', $(this).attr('placeholder'));
+    $(this).attr('placeholder','');
+  });
+
+  $(document).on('focusout', 'input', function(){
+      $(this).attr('placeholder', $(this).attr('data-placeholder'));
+  });
 });
+    
